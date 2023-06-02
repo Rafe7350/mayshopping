e1148de92925103d5c93be948ce158caa4e3e4ac
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-06-02 22:41:39 +0530

    add

---
+++ script.js
@@ -0,0 +1,9 @@
+//redirecting to login page
+document.querySelector("#login-btn").addEventListener("click", ()=>{
+    window.location.href="./login/index.html"
+  })
+  //redirecting to signup page
+  document.querySelector("#signup-btn").addEventListener("click", ()=>{
+    window.location.href="./signup/index.html"
+  })
+  
